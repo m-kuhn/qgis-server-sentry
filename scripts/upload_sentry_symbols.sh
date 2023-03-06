0a4fc8478d43aa52f38dbf691b0660d53c72903f
Matthias Kuhn <matthias@opengis.ch>
2023-03-06 04:12:47 +0100

    Upload more debug symbols

--- scripts/upload_sentry_symbols.sh
+++ scripts/upload_sentry_symbols.sh
@@ -2,4 +2,4 @@
 apt-get update
 apt-get install -y curl
 curl -sL https://sentry.io/get-cli/ | bash
-sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif -o opengisch -p bafu-sam /usr/lib/qgis/*/lib*.so
+sentry-cli --auth-token ${SENTRY_AUTH_TOKEN} upload-dif -o opengisch -p bafu-sam /usr/lib/qgis/*/lib*.so /usr/lib/libqgis*.so.*
